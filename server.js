a68745a4e63f4466cba0052b8addca7caae7793d
Zenpanic <sebastien.belmo@protonmail.com>
2020-11-20 22:37:36 +0100

    bug fixes

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const cors = require('cors');
+const nodemailer = require('nodemailer');
+const smtpTransport = require('nodemailer-smtp-transport');
 
 const app = express();
 
+const transporter = nodemailer.createTransport(smtpTransport({
+    service: 'gmail',
+    host: 'smtp.gmail.com',
+    auth: {
+        user: 'user.gmail',
+        pass: 'user.password'
+    }
+}));
+
 app.use(cors());
-app.use(express.static("files"));
+app.use(express.static("/files/"));
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.sendFile('./files/index.html');
+    res.sendFile('index.html');
 })
 
 app.post('/send_message', (req, res) => {
