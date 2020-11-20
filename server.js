3cdadac515a3a7280b76445f7eace6f9734a3f22
Zenpanic <sebastien.belmo@protonmail.com>
2020-11-20 23:13:01 +0100

    message feature

--- server.js
+++ server.js
@@ -5,8 +5,6 @@ const smtpTransport = require('nodemailer-smtp-transport');
 
 const app = express();
 
-
-
 app.use(cors());
 app.use(express.static("./files/"));
 app.use(express.json());
@@ -16,15 +14,34 @@ app.get('/', (req, res) => {
 })
 
 app.post('/send_message', (req, res) => {
+
     const transporter = nodemailer.createTransport(smtpTransport({
         service: 'gmail',
         host: 'smtp.gmail.com',
         auth: {
-            user: 'user.gmail',
-            pass: 'user.password'
+            user: process.env.GMAIL_LOGIN,
+            pass: process.env.GMAIL_PASSWORD
         }
     }));
-    res.json("Message Sent!");
+
+    const { email, message } = req.body;
+
+    const mailOptions = {
+        from: process.env.GMAIL_LOGIN,
+        to: process.env.RECEIVER,
+        subject: 'Nouveau message !',
+        message: `Nouveau message from ${email}: ${message}`
+    }
+
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
+
+    res.sendFile('index.html');
 })
 
 app.listen(process.env.PORT);
