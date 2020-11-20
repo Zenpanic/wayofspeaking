5c24b9bfdb8f9e5a9e859431ee0d6589fa79fe76
Zenpanic <sebastien.belmo@protonmail.com>
2020-11-21 00:08:11 +0100

    more bug fixes

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.static("./files/"));
+app.use(express.urlencoded({
+    extended: true
+}))
 
 app.get('/', (req, res) => {
     res.sendFile('index.html');
